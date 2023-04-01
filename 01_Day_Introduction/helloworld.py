de1e32437830d55f6f24deedeb547a1306a76f94
Orkuncuk <123820259+Orkuncuk@users.noreply.github.com>
2023-04-02 01:14:21 +0300

    Update helloworld.py
    Türkçeye çevirme.

--- 01_Day_Introduction/helloworld.py
+++ 01_Day_Introduction/helloworld.py
@@ -1,21 +1,21 @@
 # Introduction
 # Day 1 - 30DaysOfPython Challenge
 
-print(2 + 3)   # addition(+)
-print(3 - 1)   # subtraction(-)
-print(2 * 3)   # multiplication(*)
-print(3 / 2)   # division(/)
-print(3 ** 2)  # exponential(**)
-print(3 % 2)   # modulus(%)
-print(3 // 2)  # Floor division operator(//)
+print(2 + 3)   # addition(+)[toplama]
+print(3 - 1)   # subtraction(-)[çıkarma]
+print(2 * 3)   # multiplication(*)[çarpma]
+print(3 / 2)   # division(/)[bölme]
+print(3 ** 2)  # exponential(**)[üs alma]
+print(3 % 2)   # modulus(%)[bölmedeki kalanı veriyor]
+print(3 // 2)  # Floor division operator(//)[bölmedeki bölümü veriyor]
 
 # Checking data types
 
-print(type(10))                  # Int
-print(type(3.14))                # Float
-print(type(1 + 3j))              # Complex
-print(type('Asabeneh'))          # String
-print(type([1, 2, 3]))           # List
-print(type({'name':'Asabeneh'})) # Dictionary
-print(type({9.8, 3.14, 2.7}))    # Set
-print(type((9.8, 3.14, 2.7)))    # Tuple
+print(type(10))                  # Int[tam sayı]
+print(type(3.14))                # Float[doğal sayı]
+print(type(1 + 3j))              # Complex[anladığım kadarıyla denklenimsi şeyler]
+print(type('Asabeneh'))          # String[metin]
+print(type([1, 2, 3]))           # List[liste]
+print(type({'name':'Asabeneh'})) # Dictionary[sözlük]
+print(type({9.8, 3.14, 2.7}))    # Set[küme]
+print(type((9.8, 3.14, 2.7)))    # Tuple[demet]
